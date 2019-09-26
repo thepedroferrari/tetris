13d0ee12eb3513f622a72981a49ad209929cdfd5
Pedro Ferrari <pedro@pedroferrari.com>
2019-09-27 01:12:49 +0200

    Create base components

---
+++ src/components/Cell.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+interface CellProps {
+  type: string
+}
+
+const Cell: FunctionComponent<CellProps> = ({ type }) => {
+  return <div>CELL: ${type}</div>
+}
+
+export default Cell
